a952380899c314c5fbbdab2ff97ff12a82534aec
TheLouK <8thelouk8@gmail.com>
2014-03-10 21:30:52 +0000

    Keep adding code

--- js/risket.js
+++ js/risket.js
@@ -1,58 +1,110 @@
+var first = false;
 function getWar() {
+    if (first) {
+        $( ".batallon_att" ).finish();
+        $( ".batallon_def" ).finish();
+        $( ".batallon_att" ).animate({
+            left: 290
+        }, 50);
+        $( ".batallon_def" ).animate({
+            right: 290
+        }, 50);
+    }
     atacante = $( ".batallon_att .troops" ).html();
     defensor = $( ".batallon_def .troops" ).html();
+    first = true;
     if (defensor > 1 && atacante > 3) {
-        while (atacante > 3 && defensor > 1) {
-            att = [Math.floor(Math.random()*6+1), Math.floor(Math.random()*6+1), Math.floor(Math.random()*6+1)];
-            defensa = [Math.floor(Math.random()*6+1), Math.floor(Math.random()*6+1)];
-            faceWar(att, defensa);
-            $( ".draggable" ).filter("#"+id).animate({
-        }
+        att = [Math.floor(Math.random()*6+1), Math.floor(Math.random()*6+1), Math.floor(Math.random()*6+1)];
+        defensa = [Math.floor(Math.random()*6+1), Math.floor(Math.random()*6+1)];
+        faceWar(att, defensa);
+        setTimeout(function () {
+            if (defensor > 0) {
+                getWar();
+            }
+        }, 10);
     }
-    if (defensor > 0 && atacante > 3) {
-        while (atacante > 3 && defensor > 1) {
-            att = [Math.floor(Math.random()*6+1), Math.floor(Math.random()*6+1), Math.floor(Math.random()*6+1)];
-            defensa = [Math.floor(Math.random()*6+1)];
-            faceWar(att, defensa);
-            console.log("Atacante: " + att);
-            console.log("Defensor: " + defensa);
-        }
+    else if (defensor == 1 && atacante > 3) {
+        att = [Math.floor(Math.random()*6+1), Math.floor(Math.random()*6+1), Math.floor(Math.random()*6+1)];
+        defensa = [Math.floor(Math.random()*6+1)];
+        faceDoubleWarAtt(att, defensa);
+        setTimeout(function () {
+            if (defensor > 0) {
+                getWar();
+            }
+        }, 10);
     }
-    if (defensor > 0 && atacante > 2) {
-        while (atacante > 2 &&  defensor > 0) {
-            att = [Math.floor(Math.random()*6+1), Math.floor(Math.random()*6+1)];
-            defensa = [Math.floor(Math.random()*6+1)];
-            faceSingleWar(att, defensa);
-            console.log("Atacante: " + att);
-            console.log("Defensor: " + defensa);
-        }
+    else if (defensor > 1 && atacante == 3) {
+        att = [Math.floor(Math.random()*6+1), Math.floor(Math.random()*6+1)];
+        defensa = [Math.floor(Math.random()*6+1), Math.floor(Math.random()*6+1)];
+        faceWar(att, defensa);
+        setTimeout(function () {
+            if (defensor > 0) {
+                getWar();
+            }
+        }, 10);
     }
-    if (defensor > 0 && atacante == 2) {
-        while (atacante > 1 &&  defensor > 0) {
-            att = [Math.floor(Math.random()*6+1)];
-            defensa = [Math.floor(Math.random()*6+1)];
-            faceSingleWar(att, defensa);
-            console.log("Atacante: " + att);
-            console.log("Defensor: " + defensa);
-        }
+    else if (defensor == 1 && atacante == 3) {
+        att = [Math.floor(Math.random()*6+1), Math.floor(Math.random()*6+1)];
+        defensa = [Math.floor(Math.random()*6+1)];
+        faceDoubleWarAtt(att, defensa);
+        setTimeout(function () {
+            if (defensor > 0) {
+                getWar();
+            }
+        }, 10);
     }
-    finishWar(atacante, defensor);
+    else if (defensor == 1 && atacante == 2) {
+        att = [Math.floor(Math.random()*6+1)];
+        defensa = [Math.floor(Math.random()*6+1)];
+        faceSingleWar(att, defensa);
+        setTimeout(function () {
+            if (defensor > 0) {
+                getWar();
+            }
+        }, 10);
+    }
+    else if (defensor > 1 && atacante == 2) {
+        att = [Math.floor(Math.random()*6+1)];
+        defensa = [Math.floor(Math.random()*6+1), Math.floor(Math.random()*6+1)];
+        faceDoubleWarDef(att, defensa);
+        setTimeout(function () {
+            if (defensor > 0) {
+                getWar();
+            }
+        }, 10);
+    }
+    setWar(atacante, defensor);
+    if (defensor == 0 || atacante == 1) {
+        finishWar(atacante, defensor);
+    }
 }
+
 function faceWar(ataque, defensa) {
     max_att = Math.max.apply( Math, ataque );
     min_att = Math.min.apply( Math, ataque );
     max_def = Math.max.apply( Math, defensa );
     min_def = Math.min.apply( Math, defensa );
-    if (max_att > max_def || min_att > min_def) {
+    if (max_att > max_def) {
         defensor -= 1;
     }
-    else if (max_att < max_def || min_att < min_def) {
+    else if (max_att < max_def) {
         atacante -= 1;
     }
-    else if (max_att == max_def || min_att == min_def) {
+    else if (max_att == max_def) {
         atacante -= 1;
         defensor -= 1;
     }
+    if (min_att > min_def) {
+        defensor -= 1;
+    }
+    else if (min_att < min_def) {
+        atacante -= 1;
+    }
+    else if (min_att == min_def) {
+        atacante -= 1;
+        defensor -= 1;
+    }
+    setWar(atacante, defensor);
 }
 function faceSingleWar(ataque, defensa) {
     if (ataque > defensa) {
@@ -68,9 +120,78 @@ function faceSingleWar(ataque, defensa) {
     else {
         console.log("error");
     }
+    setWar(atacante, defensor);
+}
+function faceDoubleWarAtt(ataque, defensa) {
+    max_att = Math.max.apply( Math, ataque );
+    if (max_att > defensa) {
+        defensor -= 1;
+    }
+    else if (max_att < defensa) {
+        atacante -= 1;
+    }
+    else if (max_att == defensa) {
+        atacante -= 1;
+        defensor -= 1;
+    }
+    else {
+        console.log("error");
+    }
+    setWar(atacante, defensor);
+}
+function faceDoubleWarDef(ataque, defensa) {
+    max_def = Math.max.apply( Math, defensa );
+    if (ataque > max_def) {
+        defensor -= 1;
+    }
+    else if (ataque < max_def) {
+        atacante -= 1;
+    }
+    else if (ataque == max_def) {
+        atacante -= 1;
+        defensor -= 1;
+    }
+    else {
+        console.log("error");
+    }
+    setWar(atacante, defensor);
+}
+
+function setWar(atacante, defensor) {
+    $( ".batallon_att" ).animate({
+        left: 435,
+        borderTopRightRadius: 10,
+        borderBottomRightRadius: 10
+    }, 50, function() {
+        $( ".batallon_att .troops" ).text(atacante).fadeIn();
+    });
+    $( ".batallon_def" ).animate({
+        right: 435,
+        borderTopLeftRadius: 10,
+        borderBottomLeftRadius: 10
+    }, 50, function() {
+        $( ".batallon_def .troops" ).text(defensor).fadeIn();
+    });
 }
 
 function finishWar(atacante, defensor) {
-    $( ".batallon_att .troops" ).html(atacante);
-    $( ".batallon_def .troops" ).html(defensor);
+    $( ".batallon_att" ).animate({
+        left: 290,
+        borderTopRightRadius: 100,
+        borderBottomRightRadius: 100
+    }, 500);
+    $( ".batallon_def" ).animate({
+        right: 290,
+        borderTopLeftRadius: 100,
+        borderBottomLeftRadius: 100
+    }, 500);
 }
+
+$(function() {
+    $("#defensor").keyup(function() {
+        $( ".batallon_def .troops" ).text($( this ).val());
+    });
+    $("#atacante").keyup(function() {
+        $( ".batallon_att .troops" ).text($( this ).val());
+    });
+});
